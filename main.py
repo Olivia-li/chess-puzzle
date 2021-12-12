a2c5f91d48e24cbe51b68be03bb1d43c9618bfcc
Olivia Li <oliviali.ouyang@gmail.com>
2021-12-12 01:36:20 -0800

    add more helper functions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
-# Define all the shapes on the board
+import itertools
+
+# ----- Board + Shape Generation -----
 board = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
@@ -23,8 +25,8 @@ hat = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 0, 2],
-    [0, 0, 0, 2, 2, 2],
+    [0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 1],
 ]
 
 zig_zag = [
@@ -32,8 +34,8 @@ zig_zag = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
-    [0, 0, 3, 3, 0, 0],
-    [0, 0, 0, 3, 3, 3],
+    [0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 1, 1, 1],
 ]
 
 straight = [
@@ -42,16 +44,16 @@ straight = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
-    [0, 4, 4, 4, 4, 4],
+    [0, 1, 1, 1, 1, 1],
 ]
 
 cross = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 5, 0],
-    [0, 0, 0, 5, 5, 5],
-    [0, 0, 0, 0, 5, 0],
+    [0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 1, 0],
 ]
 
 t_shape = [
@@ -59,20 +61,21 @@ t_shape = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 6, 0, 0],
-    [0, 0, 6, 6, 6, 6],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 1, 1, 1],
 ]
 
 cube_ish = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 7],
-    [0, 0, 0, 0, 7, 7],
-    [0, 0, 0, 0, 7, 7],
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 1, 1],
 ]
 
 
+# ----- Shape Generation Helper Functions -----
 def num_empty_array(shape):
     count = 0
     for row in shape:
@@ -91,3 +94,35 @@ def fliplr(shape):
     new_shape = np.fliplr(shape)
     new_shape = np.roll(new_shape, shift_num, axis=1)
     return new_shape
+
+# ----- Data Type Conversions -----
+def array_to_binary(shape):
+    flat_shape = list(itertools.chain(*shape))
+    string = ''.join(map(str, flat_shape))
+    binary = int(string, 2)
+    return binary
+
+def binary_to_array(binary):
+    string = format(binary, "b")
+    string = (36-len(string)) * "0" + string
+    array = []
+
+    for i in range(6):
+        temp = list(string[i*6:i*6+6])
+        array.append(list(map(int, temp)))
+    return array 
+
+# ----- Validations -----
+# Check if the shape is out of bounds by wrapping around the board
+def is_out_of_bounds(shape):
+    if len([i[0] for i in shape]) > 0 and len([i[5] for i in shape]) > 0:
+        return True
+    else:
+        return False
+
+# Check if the shape is overlapping with other shapes 
+def has_overlap(board, shape):
+    if board & shape > 0: # bitwise and
+        return True
+    else:
+        return False
